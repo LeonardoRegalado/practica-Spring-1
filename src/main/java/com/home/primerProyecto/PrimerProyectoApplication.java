4cf4815bd4528a8f1e2ad79370cf84c4b2144b64
Leonardo <leonardoalev@gmail.com>
2025-01-29 19:52:06 -0600

    Ruta raiz agregada en la pagina principal

--- src/main/java/com/home/primerProyecto/PrimerProyectoApplication.java
+++ src/main/java/com/home/primerProyecto/PrimerProyectoApplication.java
@@ -3,11 +3,13 @@ package com.home.primerProyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/principal")
 public class PrimerProyectoApplication {
 
 	public static void main(String[] args) {
