8b756a051ab1bc78572bba3c54d0ffc972f96570
Leonardo <leonardoalev@gmail.com>
2025-01-30 22:59:51 -0600

    Aqui implemente una segunda alternativa de solucion para detectar palindromos

--- src/main/java/com/home/primerProyecto/controllers/PalindromoController.java
+++ src/main/java/com/home/primerProyecto/controllers/PalindromoController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //Decorardor de la clase 
 @RequestMapping("/empresa")
 
+//ESTA CLASE ESTA HECHA PARA PODER RECONOCER PALINDROMOS APARTIR DE LAS VARIABLES OBTENIDAS
+// DESDE LA RUTA
 public class PalindromoController {
 
-
+    //ESTE METODO INVIERTE LA PALABRA Y LA COMPARA CON LA ORIGINAL PARA SABER SI ES UN PALINDROMOS
     @GetMapping("/palindromo/{palabra}")
     public String verificarPalindromo(@PathVariable String palabra) {
         if (palabra.equals(invertir(palabra))) {
@@ -31,7 +33,27 @@ public class PalindromoController {
         }   
         return new String(cadenaInvertida) ;
     }
-    
+
+// ESTE METODO UTILIZA DOS INDICES PARA PODER RECORRER LA PALABRA 
+// DESDE EL INICIO Y UNA DESDE EL FINAL, COMPARANDO QUE LAS LETRAS EN AMBOS
+// INDICES SEAN IGUALES Y ASI DECIDIR SI ES UN PALINDROMO
+    @GetMapping("/palindrome/{palabra}")
+    public String verificarPalindromo2(@PathVariable String palabra) {
+        return verirficarPalabra(palabra);  
+    }
+
+    public  String verirficarPalabra(String palabra){
+        
+        char[] arrayPalabra = palabra.toCharArray();
+        int index = palabra.length() /2 ;
+        if (palabra.length() % 2 == 0) index ++; 
+        for (int i = 0; i < index; i++) {
+            if (arrayPalabra[i] != arrayPalabra[ palabra.length()-1-i]) {
+                return "La palabra "+palabra+" NO es un palindromo";
+            }
+        }
+        return "La palabra "+palabra+" es un palindromo";
+    }
     
 }
 
